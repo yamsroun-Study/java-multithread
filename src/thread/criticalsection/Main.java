c91ee1bca574bfd7ecea70ac6475e2022ff7326f
yamsroun <yamsroun@gmail.com>
2023-07-24 01:49:05 +0900

    16. 리소스 공유와 임계 영역 개요

---
+++ src/thread/criticalsection/Main.java
@@ -0,0 +1,67 @@
+package thread.criticalsection;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecremetingThread decremetingThread = new DecremetingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decremetingThread.start();
+
+        incrementingThread.join();
+        decremetingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
+    static class DecremetingThread extends Thread {
+
+        private final InventoryCounter inventoryCounter;
+
+        public DecremetingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    static class IncrementingThread extends Thread {
+
+        private final InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    static class InventoryCounter {
+
+        private int items = 0;
+
+        public void increment() {
+            items++;
+        }
+
+        public void decrement() {
+            items--;
+        }
+
+        public int getItems() {
+            return items;
+        }
+    }
+}
